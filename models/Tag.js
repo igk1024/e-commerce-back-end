698cdcb5ace70210658158d9ef8a6f1193687895
Ida <67982739+igk1024@users.noreply.github.com>
2020-10-23 22:25:36 -0500

    updated tag model

--- models/Tag.js
+++ models/Tag.js
@@ -7,6 +7,15 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER, //Integer
+      allowNull: false, //Doesn't allow null values
+      primaryKey: true, //Set as primary key
+      autoIncrement: true //Uses auto increment
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+      },
   },
   {
     sequelize,
