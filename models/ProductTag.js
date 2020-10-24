f0fc6b5cc77fea5dca9146a482d99d8a2062c7ad
Ida <67982739+igk1024@users.noreply.github.com>
2020-10-23 22:20:14 -0500

    product tag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,25 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER, //Integer
+      allowNull: false, //Doesn't allow null values
+      primaryKey: true, //Set as primary key
+      autoIncrement: true //Uses auto increment
+    },
+    product_id: {
+      type: DataTypes.INTEGER, //Integer
+      references: {
+        model: 'product',
+        key: 'id'
+      },
+      tag_id: {
+        type: DataTypes.INTEGER, //Integer
+        references: {
+          model: 'tag',
+          key: 'id'
+        }
+    },
   },
   {
     sequelize,
